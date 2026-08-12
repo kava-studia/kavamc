2d9b13aa5c34f33fb9aab12414f001d9b2ef0417
kava <yukava25@gmail.com>
2026-08-12 06:12:05 +0300

    Разделить канал KAVA Event и прямую связь

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,7 @@
 export const links = {
-  telegram: "https://t.me/kava_event",
+  telegram: "https://t.me/kava_studia",
+  telegramUsername: "kava_studia",
+  telegramChannel: "https://t.me/kava_event",
   vk: "https://vk.ru/kava_studia",
   max: "https://max.ru/u/f9LHodD0cOLW7UWF8xE7u4aIwludInPBOduHPLJ00Aa3Hqdcrk11mIS0oc0",
   nado: "https://nado-prazdnik.vercel.app",
