353dcd813420e7672e2a27374a681354a8475c6d
kava <yukava25@gmail.com>
2026-08-12 05:42:17 +0300

    Зафиксировать актуальные ссылки KAVA MC

---
+++ src/data/links.ts
@@ -0,0 +1,9 @@
+export const links = {
+  telegram: "https://t.me/kava_event",
+  vk: "https://vk.ru/kava_studia",
+  max: "https://max.ru/u/f9LHodD0cOLW7UWF8xE7u4aIwludInPBOduHPLJ00Aa3Hqdcrk11mIS0oc0",
+  nado: "https://nado-prazdnik.vercel.app",
+  phone: "+79932542217",
+  phoneLabel: "+7 993 254-22-17",
+  email: "juri.kava@yandex.ru",
+} as const;
